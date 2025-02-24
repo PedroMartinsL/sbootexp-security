1679d1212526ddab538cd0cafcbbe7123224133f
Pedro Martins <122813242+PedroMartinsL@users.noreply.github.com>
2025-02-24 10:16:40 -0300

    Access control

--- src/main/java/io/github/pedromartinsl/sbootexp_security/api/FooController.java
+++ src/main/java/io/github/pedromartinsl/sbootexp_security/api/FooController.java
@@ -1,6 +1,7 @@
 package io.github.pedromartinsl.sbootexp_security.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class FooController {
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> adminRoute() {
         return ResponseEntity.ok("Admin route ok!");
     }
